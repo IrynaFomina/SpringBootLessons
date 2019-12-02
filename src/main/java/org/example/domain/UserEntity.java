b5bb9b40af51a76ac9a97a03bdb63bf1d7090604
IrynaFomina <onlymyy@gmail.com>
2019-12-02 08:27:49 +0200

    second lesson : postgres DB (create new user)

---
+++ src/main/java/org/example/domain/UserEntity.java
@@ -0,0 +1,50 @@
+package org.example.domain;
+
+import org.hibernate.annotations.GeneratorType;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String password;
+
+
+    public UserEntity() {
+    }
+
+    public UserEntity(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
